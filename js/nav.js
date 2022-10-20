018eac8f74cae27cf71e22f76ba196742a75a8db
jeremie-24 <101289840+jeremie-24@users.noreply.github.com>
2022-10-20 08:54:21 -0400

    ,

---
+++ js/nav.js
@@ -0,0 +1,24 @@
+$( document ).ready(()=>{
+    $("#nav").append(content);
+});
+
+let content = `
+<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+    <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="index.html">CyberSecureTips</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="virus.html">Virus</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="passwordSecurity.html">Password
+                        Security</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>`;
